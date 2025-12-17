09e5bcabf96e625fe827b160de50159b13075105
Ritesh Kumar Panda <ritesh.ku.panda.925@gmail.com>
2025-12-17 23:02:53 +0530

    17th Dec completed

---
+++ Stacks_and_Queues/Monotonic_Stack_Queue/next_smaller_element.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> nextGreaterElement(vector<int> arr){
+    int n = arr.size();
+    vector<int> ans(n,-1);
+    stack<int> st;
+    for(int i=0;i<n;++i){
+        while(!st.empty() && arr[st.top()] > arr[i]){
+            ans[st.top()] = arr[i];
+            st.pop();
+        }
+        st.push(i);
+    }
+    return ans;
+}
+
+int main(){
+    vector<int> arr = {4, 5, 2, 10, 8};
+    vector<int> ans = nextGreaterElement(arr);
+    for(int x : ans){
+        cout<<x<<" ";
+    } cout<<endl;
+    return 0;
+}
